79a51dfe9398e7b049071f305ba1f18ad2bfd967
Andrei-Alin Socoteala <95772732+Andreii1414@users.noreply.github.com>
2025-07-30 11:51:42 +0300

    Update api_routes.py

--- app/routes/api_routes.py
+++ app/routes/api_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify, render_template
+from flask import jsonify, render_template
 from app.controllers import math_controller
 
 controller = math_controller.MathController()
+
 
 def register_routes(app):
     @app.route("/ping", methods=["GET"])
@@ -11,14 +12,12 @@ def register_routes(app):
         """
         return jsonify({"message": "pong"}), 200
 
-
     @app.route("/api/fibonacci", methods=["POST"])
     def fibonacci():
         """
         Endpoint to calculate the Fibonacci number for a given input.
         """
         return controller.fibonacci()
-
 
     @app.route("/api/power", methods=["POST"])
     def power():
@@ -27,14 +26,12 @@ def register_routes(app):
         """
         return controller.power()
 
-
     @app.route("/api/factorial", methods=["POST"])
     def factorial():
         """
         Endpoint to calculate the factorial of a given input.
         """
         return controller.factorial()
-
 
     @app.route("/api/requests", methods=["GET"])
     def get_all_requests():
@@ -44,14 +41,12 @@ def register_routes(app):
         """
         return controller.get_requests()
 
-
     @app.route("/", methods=["GET"])
     def index():
         """
         Render the main UI page.
         """
         return render_template("index.html")
-
 
     @app.route("/api/logs", methods=["GET"])
     def get_logs():
